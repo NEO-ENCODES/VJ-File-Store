7f0548aa36bdf220dbeae28ff7a5b6ca129a16d1
NEO-ENCODES <alpha369b@gmail.com>
2024-12-30 18:43:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+URL = environ.get("URL", "https://tory-andria-file-store-bot-neo-7c0720cd.koyeb.app/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
